4b44657c1bd07b1d07904584254727b46969df9a
kiseon77 <kieson.han77@gmail.com>
2024-11-19 14:04:01 +0900

    monorepo stucy

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  root: true,
+  extends: ['prettier'],
+  plugins: [],
+  rules: {
+    'prettier/prettier': 'error', // Prettier 규칙을 ESLint 규칙으로 추가
+  },
+};
---
+++ package/project1/src/App.tsx
@@ -0,0 +1,7 @@
+import './App.css';
+import str from '@monorepo/common';
+function App() {
+  return <>{str}</>;
+}
+
+export default App;
